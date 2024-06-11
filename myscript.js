02762d1ce208336b5eee8259538aeab7b1c93e48
ahillss <5051345+ahillss@users.noreply.github.com>
2024-06-12 00:49:57 +1000

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,38 +1,40 @@
-(()=>{     
+(()=>{
     function pause_all() {
         document.querySelectorAll("video").forEach(function(v){
             var p=v.parentNode.parentNode;
-            p.pauseVideo();
+            //p.playVideo();
+            //p.pauseVideo();
+            p.cueVideoById(p.getVideoData().video_id,p.getCurrentTime());
         });
     }
 
     function go() {
-        //if(!location.pathname.startsWith('/watch')) {
-        //    return;
-        //}
-        
-        pause_all();     
-        
-        setTimeout(function(){
-            pause_all();
-        }, 1000);
-        
-        setTimeout(function(){
-            pause_all();
-        }, 2000);
     
-        /*var c4_player=document.querySelector("#c4-player");
-        if(c4_player){
-            c4_player.pauseVideo();
-            c4_player.stopVideo();
-            c4_player.playVideo = () => {};
+        if(!location.pathname.match("(^/watch.*$)|(^/[@uc].*$)")) {
+            return;
         }
-        if(typeof movie_player !== 'undefined') {
-            movie_player.pauseVideo();         
-        }*/
+        
+        document.querySelectorAll("video").forEach(function(v){
+            var p=v.parentNode.parentNode;
+            //p.cueVideoById(p.getVideoData().video_id,p.getCurrentTime());
+            //p.loadVideoById(p.getVideoData().video_id,p.getCurrentTime());
+            //let playVideo = p.playVideo;
+            //p.playVideo = () => {};
+            //playVideo();
+            //p.stopVideo();
+            //
+            
+            //p.pauseVideo();
+            
+            p.seekTo(p.getCurrentTime(), true);
+            p.pauseVideo();
+        });
+        
+        //pause_all();
+        //setTimeout(function(){ pause_all(); }, 100);
     }
     
-    //document.addEventListener('yt-navigate-start', go);
+    document.addEventListener('yt-navigate-start', go);
     document.addEventListener('yt-navigate-finish', go);
     go();
 })();
